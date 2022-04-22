2de0f0f3bd9afa0822b274953c657d5e1104d00d
Michal Radomski <michalra@op.pl>
2022-04-23 01:41:52 +0200

    Temp Commit2

--- server/webpack.server.js
+++ server/webpack.server.js
@@ -14,8 +14,8 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/, //* For .js files only
-        // test: /\.(jsx|js)?$/, //* For .jsx|.js files
+        // test: /\.js$/, //* For .js files only
+        test: /\.(jsx|js)?$/, //* For .jsx|.js files
         use: "babel-loader",
         exclude: /node_modules/,
         options: {
