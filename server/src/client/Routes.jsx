48a94084dbdaba7d46793d360f66f94ea9057093
Michal Radomski <michalra@op.pl>
2022-04-25 20:23:37 +0200

    SSR Routes - commit 1

--- server/src/client/Routes.jsx
+++ server/src/client/Routes.jsx
@@ -1,15 +1,29 @@
-import React from "react";
-import {Route} from "react-router-dom";
+// import React from "react";
+// import {Route} from "react-router-dom";
 
 import Home from "./components/Home";
 import UsersList from "./components/UsersList";
 
-export default () => {
-  return (
-    <div>
-      <Route path="/" exact={true} component={Home} />
-      {/* <Route path="/hi" component={()=> "Hi"} /> */}
-      <Route path="/users" component={UsersList} />
-    </div>
-  );
-};
+//* Traditional - not working on SSR
+// export default () => {
+//   return (
+//     <div>
+//       <Route path="/" exact={true} component={Home} />
+//       {/* <Route path="/hi" component={()=> "Hi"} /> */}
+//       <Route path="/users" component={UsersList} />
+//     </div>
+//   );
+// };
+
+//* SSR Routing with React Router Config Library
+export default [
+  {
+    path: "/",
+    component: Home,
+    exact: true,
+  },
+  {
+    path: "/users",
+    component: UsersList,
+  },
+];
