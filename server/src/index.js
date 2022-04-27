c56d9f4c209a27fc516ec66df2502953b4b26360
Michal Radomski <michalra@op.pl>
2022-04-27 21:13:24 +0200

    express-http-proxy

--- server/src/index.js
+++ server/src/index.js
@@ -7,12 +7,23 @@
 import "babel-polyfill";
 import express from "express";
 import {matchRoutes} from "react-router-config";
+import proxy from "express-http-proxy";
 
 import renderer from "./helpers/renderer";
 import createStore from "./helpers/createStore";
 import Routes from "./helpers/routes";
 
 const app = express();
+
+app.use(
+  "/api",
+  proxy("http://react-ssr-api.herokuapp.com/users", {
+    proxyReqOptDecorator(opts) {
+      opts.header["x-forwarded-host"] = "localhost: 3000";
+      return opts;
+    },
+  })
+);
 
 app.use(express.static("public"));
 
