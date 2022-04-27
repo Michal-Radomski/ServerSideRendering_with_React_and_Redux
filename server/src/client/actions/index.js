75d2e29b605170bb1b5c2843d3fb3b476bddc07f
Michal Radomski <michalra@op.pl>
2022-04-28 01:10:10 +0200

    authReducer was added

--- server/src/client/actions/index.js
+++ server/src/client/actions/index.js
@@ -2,9 +2,18 @@ export const FETCH_USERS = "fetch_users";
 
 export const fetchUsers = () => async (dispatch, _getState, axiosInstance) => {
   const res = await axiosInstance.get("/users");
-
   dispatch({
     type: FETCH_USERS,
+    payload: res,
+  });
+};
+
+export const FETCH_CURRENT_USER = "fetch_current_user";
+
+export const fetchCurrentUser = () => async (dispatch, _getState, axiosInstance) => {
+  const res = await axiosInstance.get("/current_user");
+  dispatch({
+    type: FETCH_CURRENT_USER,
     payload: res,
   });
 };
