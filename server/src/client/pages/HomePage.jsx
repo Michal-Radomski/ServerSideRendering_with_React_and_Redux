26427a8a591d6092b0d288ee571604558cfa1e06
Michal Radomski <michalra@op.pl>
2022-04-28 02:38:21 +0200

    Styling was added

--- server/src/client/pages/HomePage.jsx
+++ server/src/client/pages/HomePage.jsx
@@ -2,10 +2,12 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div>
-      <div>I'm the Home Component</div>
-      <button onClick={() => console.log("Hi There!")}>Press me!</button>
-    </div>
+    <di className="center-align" style={{marginTop: "200px"}}>
+      {/* <div>I'm the Home Component</div> */}
+      {/* <button onClick={() => console.log("Hi There!")}>Press me!</button> */}
+      <h3>Welcome</h3>
+      <p>Check out these awesome features</p>
+    </di>
   );
 };
 
