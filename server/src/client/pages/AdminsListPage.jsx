c09d834aec007740e929c435c1a4ce91366002a3
Michal Radomski <michalra@op.pl>
2022-04-28 21:18:40 +0200

    AdminsListPage was added

---
+++ server/src/client/pages/AdminsListPage.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import {connect} from "react-redux";
+
+import {fetchAdmins} from "../actions/index";
+
+class AdminsListPage extends React.Component {
+  componentDidMount() {
+    this.props.fetchAdmins();
+  }
+
+  renderAdmins() {
+    return this.props.fetchAdmins.map((admin) => {
+      return <li key={admin.id}>{admin.name}</li>;
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Protected list of Admins:</h3>
+        <ul>{this.renderAdmins()}</ul>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps({admins}) {
+  return {admins: admins};
+}
+
+export default {
+  component: connect(mapStateToProps, {fetchAdmins})(AdminsListPage),
+  loadData: ({dispatch}) => dispatch(fetchAdmins()),
+};
