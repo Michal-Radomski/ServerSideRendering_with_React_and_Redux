eaea87ab128856c6831c2d188c8659115595b4d9
Michal Radomski <michalra@op.pl>
2022-05-05 19:16:28 +0200

    temp commit2

--- AdditionalTasks/03/webpack.server.ts
+++ AdditionalTasks/03/webpack.server.ts
@@ -1,19 +1,29 @@
+import * as path from "path";
 const nodeExternals = require("webpack-node-externals");
-const path = require("path");
 
 module.exports = {
-  entry: "./server/index.js",
+  entry: "./server/index.tsx",
   target: "node",
   externals: [nodeExternals()],
   output: {
     path: path.resolve("server-build"),
     filename: "index.js",
   },
+  resolve: {
+    extensions: [".ts", ".tsx"],
+  },
   module: {
     rules: [
       {
         test: /\.js$/,
         use: "babel-loader",
+      },
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+        options: {
+          configFile: "tsconfig.server.json",
+        },
       },
     ],
   },
