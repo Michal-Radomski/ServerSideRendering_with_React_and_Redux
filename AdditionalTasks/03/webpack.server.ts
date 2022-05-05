f29b7ef9ade26613c43fe1a6559681c3490d0953
Michal Radomski <michalra@op.pl>
2022-05-05 21:04:28 +0200

    temp commit5

--- AdditionalTasks/03/webpack.server.ts
+++ AdditionalTasks/03/webpack.server.ts
@@ -1,69 +1,3 @@
-// const path = require("path");
-// const nodeExternals = require("webpack-node-externals");
-
-// module.exports = {
-//   entry: "./server/server.tsx",
-//   target: "node",
-//   externals: [nodeExternals()],
-//   output: {
-//     path: path.resolve("server-build"),
-//     filename: "index.js",
-//   },
-//   resolve: {
-//     extensions: [".ts", ".tsx"],
-//   },
-//   module: {
-//     rules: [
-//       // {
-//       //   test: /\.js$/,
-//       //   use: "babel-loader",
-//       // },
-//       {
-//         test: /\.tsx?$/,
-//         loader: "ts-loader",
-//         options: {
-//           configFile: "tsconfig.server.json",
-//         },
-//       },
-//     ],
-//   },
-// };
-
-// import * as path from "path";
-// const nodeExternals = require("webpack-node-externals");
-
-// module.exports = {
-//   name: "server",
-//   entry: {
-//     server: path.resolve(__dirname, "server/server.tsx"),
-//   },
-//   mode: "production",
-//   output: {
-//     path: path.resolve("server-build"),
-//     filename: "index.js",
-//   },
-//   resolve: {
-//     extensions: [".ts", ".tsx"],
-//   },
-
-//   externals: [nodeExternals()],
-//   target: "node",
-//   node: {
-//     __dirname: false,
-//   },
-//   module: {
-//     rules: [
-//       {
-//         test: /\.tsx?$/,
-//         loader: "ts-loader",
-//         options: {
-//           configFile: "tsconfig.server.json",
-//         },
-//       },
-//     ],
-//   },
-// };
-
 const path = require("path");
 const nodeExternals = require("webpack-node-externals");
 
@@ -73,7 +7,7 @@ module.exports = {
   externals: [nodeExternals()],
   output: {
     path: path.resolve("server-build"),
-    filename: "index.js",
+    filename: "server.js",
   },
   resolve: {
     extensions: [".ts", ".tsx", ".js"],
@@ -81,10 +15,9 @@ module.exports = {
   module: {
     rules: [
       // {
-      //   test: /\.tsx?$/,
-      //   use: "ts-loader",
+      //   test: /\.js$/,
+      //   use: "babel-loader",
       // },
-
       {
         test: /\.tsx?$/,
         loader: "ts-loader",
