5ff58893298a044a3f97a1bd9b59b244176fdb2c
Michal Radomski <michalra@op.pl>
2022-05-06 23:51:34 +0200

    _app.tsx types were fixed

--- AdditionalTasks/04/2 ssr/pages/_app.tsx
+++ AdditionalTasks/04/2 ssr/pages/_app.tsx
@@ -1,8 +1,13 @@
-import {AppProps} from "next/app";
-
+import React from "react";
 import "../styles/index.scss";
 
-export default function MyApp({Component, pageProps}: AppProps) {
+export default function MyApp({
+  Component,
+  pageProps,
+}: {
+  Component: typeof React.Component;
+  pageProps: typeof Object;
+}): JSX.Element {
   console.log("Component:", Component);
   console.log("pageProps:", pageProps);
 
